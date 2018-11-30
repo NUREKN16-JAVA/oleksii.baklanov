5ee17c573a7570a0c1a935512b94f2d3fe5a202d
bezcontrol <oleksii.baklanov@nure.ua>
2018-11-30 19:21:26 +0200

    adding database

---
+++ src/main/java/baklanov/usermanagement/db/ConnectionFactory.java
@@ -0,0 +1,7 @@
+package baklanov.usermanagement.db;
+
+import java.sql.Connection;
+
+public interface ConnectionFactory {
+    Connection createConnection() throws DatabaseException;
+}
---
+++ src/main/java/baklanov/usermanagement/db/UserDao.java
@@ -0,0 +1,19 @@
+package baklanov.usermanagement.db;
+
+import baklanov.usermanagement.User;
+
+import java.util.Collection;
+
+public interface UserDao {
+    User create(User user) throws DatabaseException;
+
+    void update(User user) throws DatabaseException;
+
+    void delete(User user) throws DatabaseException;
+
+    User find(Long id) throws DatabaseException;
+
+    Collection findAll() throws DatabaseException;
+
+    void setConnectionFactory(ConnectionFactory connectionFactory);
+}
